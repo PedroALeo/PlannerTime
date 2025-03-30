129bb66e178ec83acf7774df55fc2840a092cb3d
Matheus Resende <matheusresende1492@gmail.com>
2025-03-30 19:48:20 -0300

    profile page

--- app/plannerTime/src/App.tsx
+++ app/plannerTime/src/App.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage.tsx'
+import ProfilePage from './pages/Profile/ProfilePage.tsx'
 import Header from './components/Header/Header.tsx'
 import Login from './pages/Login/Login.tsx'
 
@@ -10,6 +11,7 @@ function App() {
           children: [
             {path: "/lp", element: <LandingPage />},
             {path: "/login", element: <Login />},
+            {path: "/profile", element: <ProfilePage />},
             //{path: "/articles", element: <Articles />},
             //{path: "/article", element: <Article />},
             //{path: "/newArticle", element: <NewArticle />},
